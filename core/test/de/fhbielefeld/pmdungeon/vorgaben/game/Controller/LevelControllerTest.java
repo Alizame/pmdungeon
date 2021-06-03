da88fb734d8152843d5f74b548703e0a44702fa2
Maximilian Frömelt <maxifroemelt@gmail.com>
2021-06-03 20:23:14 +0200

    junit5 integration

---
+++ core/test/de/fhbielefeld/pmdungeon/vorgaben/game/Controller/LevelControllerTest.java
@@ -0,0 +1,24 @@
+package de.fhbielefeld.pmdungeon.vorgaben.game.Controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class LevelControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
